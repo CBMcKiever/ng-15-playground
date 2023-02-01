6019c9b424c2aa6ec77d559e88ffd56abc74164e
Cole McKiever <colemckiever@gmail.com>
2023-02-01 17:40:35 -0600

    init form for new todo

---
+++ src/app/features/todo/todo-new.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-new',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      todo-new works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TodoNewComponent {
+
+}
